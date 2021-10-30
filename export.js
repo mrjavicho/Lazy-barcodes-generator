826462897cadfd30e5fd9ae47adefe306d94610d
Javier Fernando Huamanchumo Arauco <javier.huamanchumo@gmail.com>
2021-10-30 14:50:09 +0200

    Export fix attempt (i)

--- export.js
+++ export.js
@@ -3,7 +3,9 @@ function exportPdf() {
     var currentContent = document.getElementById('barcodesContainer').innerHTML;
     window.sessionStorage.setItem('currentBarcodes', currentContent);
 
-    var printPreview = window.open('export.html');
+    const exportPath = 'https://mrjavicho.github.io/Lazy-barcodes-generator/export.html'
+    //const exportPath = 'export.html'
+    var printPreview = window.open(exportPath);
 
     printContainer.innerHTML = currentContent
     printPreview.print(); 
